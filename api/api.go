7f4802b95f2ad64df36aa4fcb365b4f1e1c8aa8c
rose <snowydg@proton.me>
2025-05-15 12:04:10 -0500

    bug fix: forgot to add middleware

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ros-e/lumi/s3"
 )
 
 func Serve(addr string, startbanner bool, debug bool) {
@@ -15,7 +16,7 @@ func Serve(addr string, startbanner bool, debug bool) {
 	fmt.Printf("[\033[35m REST API started on \033[0mhttp://%s ]\n", addr)
 	r := gin.Default()
 	r.GET("/:bucket/*key", GetObject)
-	r.GET("/:bucket", ListObjectsV2Handler)
+	r.GET("/:bucket", s3.S3AuthMiddleware(), ListObjectsV2Handler)
 	r.PUT("/:bucket/*key", PutObject)
 
 	if err := r.Run(addr); err != nil {
