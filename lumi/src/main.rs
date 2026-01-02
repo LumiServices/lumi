180dfd56fecf75a3729a3a8b859fd6c91372bc98
ros-e <snowydg@proton.me>
2026-01-02 15:56:20 -0600

    feat: basic ass REST api

--- lumi/src/main.rs
+++ lumi/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, World!");
+use::lumi_server::start_server;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    start_server(3000,"*".to_string()).await;
+    Ok(())
 }
