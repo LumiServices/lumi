3fb0bd393db0ca74d6d54d555381f81dc66d3e8d
ros-e <176725295+ros-e@users.noreply.github.com>
2026-02-07 19:17:50 -0600

    feat(bucket): add check for existing bucket

--- crates/server/src/routes/bucket/operations.rs
+++ crates/server/src/routes/bucket/operations.rs
@@ -65,6 +65,7 @@ pub async fn list_buckets() -> impl IntoResponse {
 async fn list_objects() -> impl IntoResponse {
     ErrorCode::NotImplemented.into_response()
 }
+
 pub async fn create_bucket(bucket: String) -> impl IntoResponse {
     if bucket.len() < 3 || bucket.len() > 63
         || !bucket.chars().all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '-' || c == '.')
@@ -75,32 +76,36 @@ pub async fn create_bucket(bucket: String) -> impl IntoResponse {
     }
         let path = format!("./data/{}", bucket);
         if bucket.is_empty() {
-        return ErrorCode::NoSuchBucket.into_response();
-    }
-    /*
-        <Cthon98> hey, if you type in your pw, it will show as stars
-        <Cthon98> ********* see!
-        <AzureDiamond> hunter2
-        <AzureDiamond> doesnt look like stars to me
-        <Cthon98> <AzureDiamond> *******
-        <Cthon98> thats what I see
-        <AzureDiamond> oh, really?
-        <Cthon98> Absolutely
-        <AzureDiamond> you can go hunter2 my hunter2-ing hunter2
-        <AzureDiamond> haha, does that look funny to you?
-        <Cthon98> lol, yes. See, when YOU type hunter2, it shows to us as *******
-        <AzureDiamond> thats neat, I didnt know IRC did that
-        <Cthon98> yep, no matter how many times you type hunter2, it will show to us as *******
-        <AzureDiamond> awesome!
-        <AzureDiamond> wait, how do you know my pw?
-        <Cthon98> er, I just copy pasted YOUR ******'s and it appears to YOU as hunter2 cause its your pw
-        <AzureDiamond> oh, ok.
-     */
-    if bucket == "hunter2" {
-        return ErrorCode::Hunter2.into_response();
-    }
-    match tokio::fs::create_dir_all(&path).await {
-        Ok(_) => StatusCode::OK.into_response(),
-        Err(_) => ErrorCode::BucketAlreadyExists.into_response(),
-    }
+            return ErrorCode::NoSuchBucket.into_response();
+        }
+        /*
+            <Cthon98> hey, if you type in your pw, it will show as stars
+            <Cthon98> ********* see!
+            <AzureDiamond> hunter2
+            <AzureDiamond> doesnt look like stars to me
+            <Cthon98> <AzureDiamond> *******
+            <Cthon98> thats what I see
+            <AzureDiamond> oh, really?
+            <Cthon98> Absolutely
+            <AzureDiamond> you can go hunter2 my hunter2-ing hunter2
+            <AzureDiamond> haha, does that look funny to you?
+            <Cthon98> lol, yes. See, when YOU type hunter2, it shows to us as *******
+            <AzureDiamond> thats neat, I didnt know IRC did that
+            <Cthon98> yep, no matter how many times you type hunter2, it will show to us as *******
+            <AzureDiamond> awesome!
+            <AzureDiamond> wait, how do you know my pw?
+            <Cthon98> er, I just copy pasted YOUR ******'s and it appears to YOU as hunter2 cause its your pw
+            <AzureDiamond> oh, ok.
+        */
+        if bucket == "hunter2" {
+            return ErrorCode::Hunter2.into_response();
+        }
+        // check if dat bitch already exists
+        if tokio::fs::metadata(&path).await.is_ok() {
+            return ErrorCode::BucketAlreadyExists.into_response();
+        }
+        match tokio::fs::create_dir_all(&path).await {
+            Ok(_) => StatusCode::OK.into_response(),
+            Err(_) => ErrorCode::BucketAlreadyExists.into_response(),
+        }
 }
