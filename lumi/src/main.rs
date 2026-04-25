2721d2403f95bed588e0e204329618a2e1accede
ros-e <176725295+ros-e@users.noreply.github.com>
2026-04-25 15:06:09 -0500

    chore: update packages

--- lumi/src/main.rs
+++ lumi/src/main.rs
@@ -1,11 +1,12 @@
 use clap::{Parser, Subcommand};
-use lumi_server::{http::start_http_server};
+use lumi_server::http::start_http_server;
 use lumi_utils::update::fetch_latest_github_release;
+use tokio::fs::create_dir_all;
 #[derive(Parser, Debug)]
 #[command(version = env!("CARGO_PKG_VERSION"), about = "lumi CLI")]
 struct Args {
-     #[command(subcommand)]
-     command: Commands,
+    #[command(subcommand)]
+    command: Commands,
 }
 #[derive(Subcommand, Debug)]
 enum Commands {
@@ -16,29 +17,42 @@ enum Commands {
         port: u64,
         #[arg(long, default_value = "*")]
         allowed_origin: String,
-    }, 
+    },
     Update,
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data_dir = "./data";
+    create_dir_all(data_dir).await?;
     let args = Args::parse();
     match args.command {
-        Commands::Serve { host, port, allowed_origin, } => {
-                start_http_server(host, port, allowed_origin).await?;
-                Ok(())
-            }
+        Commands::Serve {
+            host,
+            port,
+            allowed_origin,
+        } => {
+            start_http_server(host, port, allowed_origin).await?;
+            Ok(())
+        }
         Commands::Update => {
             println!("\x1b[33mChecking for updates...\x1b[0m");
             match tokio::task::spawn_blocking(|| {
                 fetch_latest_github_release().map_err(|e| e.to_string())
-            }).await {
+            })
+            .await
+            {
                 Ok(result) => match result {
                     Ok(latest) => {
                         let current = env!("CARGO_PKG_VERSION");
                         let latest = latest.trim();
                         if latest != current {
-                            println!("\x1b[36mNew version available: {} (current: {})\x1b[0m", latest, current);
-                            println!("Visit https://github.com/ros-e/lumi/releases/latest to download");
+                            println!(
+                                "\x1b[36mNew version available: {} (current: {})\x1b[0m",
+                                latest, current
+                            );
+                            println!(
+                                "Visit https://github.com/ros-e/lumi/releases/latest to download"
+                            );
                         } else {
                             println!("\x1b[32mYou are running the latest version.\x1b[0m");
                         }
